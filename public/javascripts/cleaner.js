497d71202a9da6aba58b01307761cc8a805862fd
00serg <sergiooparra@student.hartnell.edu>
2017-06-27 12:02:41 -0700

    *Created new variable dupCollection to hold duplicate values. *Adjusted if statement in order to route information and its duplicates. *Added the ability to create files and append information. ****Added writing functionality at the end in order to check both collections of information(newCollection && dupCollection).

--- public/javascripts/cleaner.js
+++ public/javascripts/cleaner.js
@@ -2,29 +2,44 @@ var express = require('express');
 var fs = require('fs');
 
 function Cleaner(pinCollection){
+    var dupCollection = [];
     var newCollection = [];
     var priorPin = null;
 
-    pinCollection.forEach(function(pin){
-        if (priorPin === null){
-            priorPin = pin;
-            return;
-        } else {
-            newCollection.push(priorPin);
-        }
-
-        console.log('Comparing: ' + pin.toString() + " to " + priorPin.toString());
-        if (pin.TimeStamp == priorPin.TimeStamp && pin.latitude == priorPin.latitude
-        && pin.longitude == pin.longitude){
-             console.log("duplicate");
-            return;
-        } else {
-            priorPin = pin;
-        }
-
+    // Creates file
+    fs = require('fs');
+    fs.writeFile('../public/data/set2.txt', "", function (err) {
+        if (err)
+            return console.log(err);
     });
 
-    console.log("new Collection: " + String(newCollection.length));
-}
+    pinCollection.forEach(function(pin){
+        if (priorPin === null || pin != priorPin && pin.TimeStamp == priorPin.TimeStamp && pin.latitude == priorPin.latitude
+        && pin.longitude == priorPin.longitude){
+            priorPin = pin;
+            dupCollection.push(priorPin);
+        } else {
+            priorPin = pin;
+            newCollection.push(priorPin);
+            fs.appendFile('../public/data/set2.txt', priorPin + "\n", function (err) {
+                if (err) return console.log(err);
+            });
+        }
+    });
 
+    console.log("Duplicate Collection: " + dupCollection.length);
+    /* fs = require('fs');
+    fs.writeFile('../public/data/bad.txt', dupCollection + "\n", function (err) {
+        if (err)
+            return console.log(err);
+    }); */
+    console.log("Good Collection: " + newCollection.length);
+    /* fs = require('fs');
+    fs.writeFile('../public/data/good.txt', newCollection + "\n", function (err) {
+        if (err)
+            return console.log(err);
+    }); */
+    console.log("Duplicate + Good = " + (dupCollection.length + newCollection.length));
+    console.log("Original Collection: " + pinCollection.length);
+}
 module.exports = Cleaner;
